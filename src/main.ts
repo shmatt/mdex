41ffdaeaa3554cdea53c5be46c8061efc3bbfbb0
Matt Tew <matt.tew@intellis.team>
2024-05-06 15:26:31 +0000

    Initial version

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,60 @@
 import * as core from '@actions/core'
 import * as glob from '@actions/glob'
-import { wait } from './wait'
 import path from 'path'
 import { promises as fs } from 'fs'
+import * as fm from 'front-matter'
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-	try {
-		const rootPath = core.getInput('path') || '~/Journal'
+  try {
+    const pattern = core.getInput('pattern') || '/home/node/Journal/**/*.md'
+    const output = core.getInput('output') || 'index.json'
 
-		// Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-		const pattern = path.join(rootPath, '**/*.md');
-		core.debug(`Reading markdown files from ${pattern}`);
+    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    core.info(`Reading markdown files from ${pattern}`)
 
-		const globber = await glob.create(pattern);
-		for await (const file of globber.globGenerator()) {
-			core.debug(`Found file: ${file}`);
-			// Read the file
-			const content = await fs.readFile(file, 'utf8');
-			const stats = await fs.stat(file);
-			const fileStats = {
-				ctime: stats.ctime.getTime(),
-				mtime: stats.mtime.getTime(),
-				size: stats.size
-			};
+    const index: { [key: string]: unknown } = {}
 
-			// extract frontmatter
+    const globber = await glob.create(pattern)
+    let fileCount = 0
+    let errorCount = 0
 
-			// Parse the file
-			// core.debug(`Parsed page: ${page}`);
-		}
+    for await (const file of globber.globGenerator()) {
+      try {
+        fileCount++
 
-	} catch (error) {
-		// Fail the workflow run if an error occurs
-		if (error instanceof Error) core.setFailed(error.message)
-	}
+        core.debug(`Found file: ${file}`)
+
+        // Read the file
+        const content = await fs.readFile(file, 'utf8')
+
+        // extract frontmatter
+        const { attributes } = fm.default<unknown>(content)
+
+        // Parse the relative path based on the glob pattern
+        const relativePath = path.relative(globber.getSearchPaths()[0], file)
+
+        // Add the file to the index
+        index[relativePath] = attributes
+      } catch (error) {
+        core.error(`Error processing file: ${file}`)
+        if (error instanceof Error) core.error(error.message)
+        errorCount++
+      }
+    }
+
+    core.info(`Processed ${fileCount} files with ${errorCount} errors`)
+
+    // Write the index to the output file
+    await fs.writeFile(output, JSON.stringify(index, null, 2))
+
+    core.info(`Wrote index to ${output}`)
+    core.setOutput('indexPath', output)
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
